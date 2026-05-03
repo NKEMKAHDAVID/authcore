511bd12224c8f8b358f91f1c295b445988d8dbfe
NKEMKAHDAVID <nkemkahchukwudubem@gmail.com>
2026-05-03 18:56:26 +0100

    fix: remove redundant annotations and unused imports in UserStateService and AuthService

--- src/main/java/com/authcore/modules/auth/AuthService.java
+++ src/main/java/com/authcore/modules/auth/AuthService.java
@@ -12,10 +12,8 @@ import com.authcore.modules.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import com.authcore.modules.user.UserStateService;
 
--- src/main/java/com/authcore/modules/user/UserStateService.java
+++ src/main/java/com/authcore/modules/user/UserStateService.java
@@ -1,17 +1,15 @@
 package com.authcore.modules.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import com.authcore.modules.user.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class UserStateService {
 
-    @Autowired
+
     private final UserRepository userRepository;
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
